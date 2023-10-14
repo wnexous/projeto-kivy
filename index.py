1364b8b3467ecaa716672dabf7d03eafa2efd176
adnegro <andre.negro@oi.net.br>
2023-10-13 21:00:22 -0300

    change text color from total a pagar

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import NumericProperty
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
 class Lanchonete(BoxLayout):
     total = NumericProperty(3)
@@ -11,9 +13,13 @@ class Lanchonete(BoxLayout):
         else:
             self.total -= valor
 
+    def show_popup(self):
+      popup = Popup(title='Compra realizada!', content=Label(text='Seu pedido foi realizado com sucesso!'), size_hint=(None,None), size=(400,400))
+      popup.open()
+
 class LanchoneteApp(App):
     def build(self):
-        return Lanchonete()
+      return Lanchonete()
 
 if __name__ == '__main__':
     LanchoneteApp().run()
