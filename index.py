1844803fbaa510d8568a6e53182f0ac0087c4d02
adnegro <andre.negro@oi.net.br>
2023-10-13 21:21:41 -0300

    end kiv project with Buy button

--- index.py
+++ index.py
@@ -1,25 +1,36 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import NumericProperty
-from kivy.uix.popup import Popup
 from kivy.uix.label import Label
+from kivy.properties import NumericProperty, ListProperty
+from kivy.uix.popup import Popup
+
 
 class Lanchonete(BoxLayout):
     total = NumericProperty(3)
+    itens = ListProperty([])
 
-    def adicionar_item(self, valor, ativo):
+    def adicionar_item(self, valor, ativo, item):
         if ativo:
             self.total += valor
+            if item not in self.itens:
+                self.itens.append(item)
         else:
             self.total -= valor
+            if item in self.itens:
+                self.itens.remove(item)
 
     def show_popup(self):
-      popup = Popup(title='Compra realizada!', content=Label(text='Seu pedido foi realizado com sucesso!'), size_hint=(None,None), size=(400,400))
-      popup.open()
+        forma_pagamento = self.ids.spinner_pagamento.text
+        popup = Popup(title='Compra realizada!', content=Label(text='Seu pedido foi realizado com sucesso!\nOs itens do seu sanduíche são: \n\n' + '\n'.join(
+            self.itens) + '\n\nForma de pagamento escolhida foi: ' + forma_pagamento), size_hint=(None, None), size=(400, 400))
+        popup.open()
+        self.itens = []
+
 
 class LanchoneteApp(App):
     def build(self):
-      return Lanchonete()
+        return Lanchonete()
+
 
 if __name__ == '__main__':
     LanchoneteApp().run()
